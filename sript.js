adbc82be6058f1ca7c37b321c0d53a98dd89ab3d
= <renatomdavid811@hotmail.com>
2024-04-24 21:14:12 -0300

    aaa

--- sript.js
+++ sript.js
@@ -1,6 +1,9 @@
 function redirecionar() {
     window.location.href = "https://www.youtube.com/shorts/3qGbunYUQ0M";
 }
+function redirecionamento() {
+    window.location.href = "https://www.youtube.com/watch?v=OB-mSJsO0T0";
+}
 const botaoMaluco = document.getElementById('botao-maluco');
 
 botaoMaluco.addEventListener('mouseenter', () => {
